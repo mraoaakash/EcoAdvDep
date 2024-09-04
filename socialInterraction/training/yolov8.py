4d23fab49480a85f017d5d1e1b69fe78124ccca9
Aakash Rao <mrao.aakash@gmail.com>
2024-09-04 11:11:52 +0530

    Added data URLs and Links

--- socialInterraction/training/yolov8.py
+++ socialInterraction/training/yolov8.py
@@ -4,16 +4,6 @@ from ultralytics import YOLO
 
 
 
-
-
-# export COMET_API_KEY=NiIALx9i86acjJL53ck558RCD
-# export COMET_WORKSPACE=zebrafish2
-# export COMET_PROJECT_NAME=zebrafish-tracking
-
-
-
-
-
 model = YOLO('yolov8m.pt') 
 
-results = model.train(data='/Users/mraoaakash/Documents/research/research-arpi/Social-interraction/algorithms/training/data.yaml', epochs=50, imgsz=640, name="First-Run", batch=8, save_period=10,device='mps', rect=True, workers=0)
+results = model.train(data='../training/data.yaml', epochs=50, imgsz=640, name="First-Run", batch=8, save_period=10,device='mps', rect=True, workers=0)
